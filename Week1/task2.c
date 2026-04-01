1f13b3050f4e63fb19e101533c8582ff772ee223
hanypark <115128374+Selene-and-Luna@users.noreply.github.com>
2026-04-01 16:48:16 +0900

    Create task2.c

---
+++ Week1/task2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int inp, result = 0;
+    for(int i = 0 ;  i < 5 ; i++){
+        /*
+          write down your codes.
+          */
+    }
+    printf("%d\n", result%10); 
+    
+    return 0;  
+}
