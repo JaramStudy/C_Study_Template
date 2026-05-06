e03a3b4d26eb4118536b3773fb8b1c1fdd4108ad
Selene-and-Luna <hanypark03@gmail.com>
2026-05-06 17:06:17 +0900

    practice update

---
+++ Week4/practice1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define MAX 31
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    // 배열 선언
+
+    // fib[0], fib[1] 초기화
+
+    // 반복문을 이용한 피보나치 수 계산
+
+    // 최종 결과 출력
+
+    return 0;
+}
---
+++ Week4/practice2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 50
+
+int main() {
+    char buffer[MAX];
+    // fgets를 사용해 줄 단위로 입력
+
+    // 출력: 각 요구사항을 한 줄 씩 출력
+    // 입력받은 문자열
+
+    // 문자열의 길이
+
+    // Jaram 이 포함되면 Yes, 아니면 No 출력
+
+    return 0;
+}
