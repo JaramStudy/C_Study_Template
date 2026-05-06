9475213abf0760fdb2878336c848c6a2f5a388a3
Selene-and-Luna <hanypark03@gmail.com>
2026-05-06 17:30:20 +0900

    Create task1.c

---
+++ Week4/task1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    int n;
+    char names[10][20];
+    char target[20];
+
+    // n 입력
+
+    // 명단 입력
+
+    // 찾고 싶은 학생 입력
+
+    // 배열을 탐색하여 해당 학생을 찾으면 이름 출력
+
+    // 만약 없다면, Not found 출력
+    
+    return 0;
+}
